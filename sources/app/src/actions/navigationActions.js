0dede42dc08427028d9c11293b1b757168a5166b
jvega190 <jose.vega@craftersoftware.com>
2018-06-06 12:30:21 -0600

    Removed axios dep not being used

--- sources/app/src/actions/navigationActions.js
+++ sources/app/src/actions/navigationActions.js
@@ -1,4 +1,3 @@
-// import axios from "axios";
 import { navService } from '../api.js';
 
 export function getNav() {
@@ -12,47 +11,3 @@ export function getNav() {
         });
     }
 }
-
-// export function fetchTweets() {
-//     return function(dispatch) {
-//         dispatch({type: "FETCH_TWEETS"});
-        
-//         /* 
-//         http://rest.learncode.academy is a public test server, so another user's experimentation can break your tests
-//         If you get console errors due to bad data:
-//         - change "reacttest" below to any other username
-//         - post some tweets to http://rest.learncode.academy/api/yourusername/tweets
-//         */
-//         axios.get("http://rest.learncode.academy/api/reacttest/tweets")
-//         .then((response) => {
-//             dispatch({type: "FETCH_TWEETS_FULFILLED", payload: response.data})
-//         })
-//         .catch((err) => {
-//             dispatch({type: "FETCH_TWEETS_REJECTED", payload: err})
-//         })
-//     }
-// }
-
-// export function addTweet(id, text) {
-//   return {
-//     type: 'ADD_TWEET',
-//     payload: {
-//       id,
-//       text,
-//     },
-//   }
-// }
-
-// export function updateTweet(id, text) {
-//   return {
-//     type: 'UPDATE_TWEET',
-//     payload: {
-//       id,
-//       text,
-//     },
-//   }
-// }
-
-// export function deleteTweet(id) {
-//   return { type: 'DELETE_TWEET', payload: id}
-// }
