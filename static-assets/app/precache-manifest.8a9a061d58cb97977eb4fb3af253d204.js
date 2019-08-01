43e0ddd88941599c3e56bc48b522cdfa0a54df34
jvega190 <jose.vega@craftersoftware.com>
2019-08-01 10:00:40 -0600

    #3306 - Build fix, updated build import in freemarker template

---
+++ static-assets/app/precache-manifest.8a9a061d58cb97977eb4fb3af253d204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48de7f98d1c3f76e24a30643b34d4732",
+    "url": "/static-assets/app/index.html"
+  },
+  {
+    "revision": "6e935c868176423e09fc",
+    "url": "/static-assets/app/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "b7f742a94bc096dff6ab",
+    "url": "/static-assets/app/static/css/main.75e8ad94.chunk.css"
+  },
+  {
+    "revision": "6e935c868176423e09fc",
+    "url": "/static-assets/app/static/js/2.4515b6e1.chunk.js"
+  },
+  {
+    "revision": "b7f742a94bc096dff6ab",
+    "url": "/static-assets/app/static/js/main.f8010d72.chunk.js"
+  },
+  {
+    "revision": "7be20a1322485b6f76f7",
+    "url": "/static-assets/app/static/js/runtime~main.c02028ff.js"
+  }
+]);
