e5c6ef3099e55aecf19600a920a3c19fe3748b4f
rart <royart@me.com>
2020-04-18 08:59:10 +0200

    [search] Elasticsearch endpoint updates
    hit.id => hit._id
    Better HLS playback
    Cleaning App source warnings
    App build

---
+++ static-assets/app/precache-manifest.39b3cc7750529c619b065b61616c8b96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49bb0bb9abf20504e41b60cace49e3cc",
+    "url": "/static-assets/app/index.html"
+  },
+  {
+    "revision": "eb9da2b508fb219d650f",
+    "url": "/static-assets/app/static/css/2.f1580a2e.chunk.css"
+  },
+  {
+    "revision": "79da1b0f8a40da485bfa",
+    "url": "/static-assets/app/static/css/main.20f91c14.chunk.css"
+  },
+  {
+    "revision": "eb9da2b508fb219d650f",
+    "url": "/static-assets/app/static/js/2.b8892884.chunk.js"
+  },
+  {
+    "revision": "6bb153e8265fb81f4600192f9f637b4b",
+    "url": "/static-assets/app/static/js/2.b8892884.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79da1b0f8a40da485bfa",
+    "url": "/static-assets/app/static/js/main.84568858.chunk.js"
+  },
+  {
+    "revision": "dd6d4cbe3a7288817943",
+    "url": "/static-assets/app/static/js/runtime-main.ecf31c5f.js"
+  }
+]);
