1da05f9a3415a1447122dc819e2b9dac9a3c8bd6
jvega190 <josevega19@gmail.com>
2021-09-23 10:39:10 -0600

    Build update

---
+++ static-assets/app/precache-manifest.a1adafc14b93042b19cb377d45ab8871.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c7382618779caf9c5b600a7cade556",
+    "url": "/static-assets/app/index.html"
+  },
+  {
+    "revision": "f4551c2311b21cb4e8ad",
+    "url": "/static-assets/app/static/css/2.f1580a2e.chunk.css"
+  },
+  {
+    "revision": "d7e3fe07935a1de09566",
+    "url": "/static-assets/app/static/css/main.20f91c14.chunk.css"
+  },
+  {
+    "revision": "f4551c2311b21cb4e8ad",
+    "url": "/static-assets/app/static/js/2.6aa6bd08.chunk.js"
+  },
+  {
+    "revision": "43b723125e202214440b731421b160a9",
+    "url": "/static-assets/app/static/js/2.6aa6bd08.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f428d0a77477e6d3b3f",
+    "url": "/static-assets/app/static/js/3.03665914.chunk.js"
+  },
+  {
+    "revision": "fc46316b4a7fe9b89753",
+    "url": "/static-assets/app/static/js/4.a34578fe.chunk.js"
+  },
+  {
+    "revision": "d7e3fe07935a1de09566",
+    "url": "/static-assets/app/static/js/main.5ca5f9b4.chunk.js"
+  },
+  {
+    "revision": "aadbb7f46e10b1d6ed05",
+    "url": "/static-assets/app/static/js/runtime-main.7cbb30e4.js"
+  }
+]);
